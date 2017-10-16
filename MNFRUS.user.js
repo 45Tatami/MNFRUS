8175258046c9b89f5b1af3a29eee00fd88b8b755
45Tatami <joshua.schauer@gmx.de>
2017-10-16 23:47:29 +0200

    Started using meguca's api instead of the DOM tree
    Only partially though, didn't find a way to get notified on new posts
    with the meguca objects.
    Also changed the formatting.

--- MNFRUS.user.js
+++ MNFRUS.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        MNFRUS
-// @namespace   gucaguca
+// @name				MNFRUS
+// @namespace		gucaguca
 // @description Meguca NameFag Removal UserScript
-// @include     https://meguca.org/*
-// @version     1.2
-// @grant       none
-// @run-at      document-end
+// @include			https://meguca.org/*
+// @version			1.2
+// @grant			none 
+// @run-at			document-end
 // ==/UserScript==
 
 // ================== CONFIG ==================
@@ -17,7 +17,7 @@ var filterList = ["Like this", "or this"];
 var hideFilterCount = false;
 
 // Set to true if you want posts to be completely invisible
-var invisibleDelete = false;
+var invisibleDelete =  false;
 
 // Set to false to disable chain filtering
 var chainFiltering = true;
@@ -27,159 +27,144 @@ var chainFiltering = true;
 var deletedPostCount = 0;
 var removedCountElement;
 var threadContainer = document.getElementById("thread-container");
-var opPost;
-var REMOVED = " mnfrus_filtered";
 var newPostObserver = null;
 var obsAttrConfig = { attributes: true, attributeFilter: [ "class" ]};
+var postList;
 
 // ================== FUNCTIONS ==================
 
 function postCreationHandler(mutationRecords) {
-  mutationRecords.forEach(function(mutation) {
-    if (mutation.type == "childList") {
-      for (var post of mutation.addedNodes) {
-        checkForRemovalByName(post);
-        if (!isFiltered(post) && post.className.includes("editing")) {
-          newPostObserver.observe(post, obsAttrConfig);
-        }
-      }
-    } else if (mutation.type == "attributes") {
-      checkForRemovalByReplies(mutation.target);
-    }
-  });
+	mutationRecords.forEach(function(mutation) {
+		if (mutation.type == "childList") {
+			for (let p of mutation.addedNodes) {
+				let post = getPostFromDOMObject(p);
+				checkForRemovalByName(post);
+				if (!isFiltered(post) && post.editing) {
+					newPostObserver.observe(p, obsAttrConfig);
+				}
+			}
+		} else if (mutation.type == "attributes") {
+			checkForRemovalByReplies(getPostFromDOMObject(mutation.target));
+		}
+	});
 }
 
 function checkForRemoval(post) {
-  checkForRemovalByName(post);
-  checkForRemovalByReplies(post);
+	checkForRemovalByName(post);
+	checkForRemovalByReplies(post);
 }
 
 // Checks if a post needs to be removed and removes it.
 // If chainfiltering is on, recurses through replies.
 function checkForRemovalByName(post) {
-  // Abort if already filtered or not a post
-  if (post == null || isFiltered(post))
-    return;
-  
-  // get Name
-  var postName = getPostName(post);
-  
-  // Go through list of filters, remove and break on match
-  for (var filterName of filterList) {
-    if (filterName == postName) {
-      removePost(post);
-      break;
-    }
-  }
+	if (isFiltered(post))
+		return;
+	
+	// get Name
+	var postName = post.name;
+	
+	// Go through list of filters, remove and break on match
+	for (var filterName of filterList) {
+		if (filterName == postName) {
+			removePost(post);
+			break;
+		}
+	}
 }
 
 function checkForRemovalByReplies(post) {
-  if (post == null || isFiltered(post))
-    return;
-  
-  var parents = getParents(post);
-  if (parents.length > 0) {
-    var shitpost = true;
-    for (var p of parents) {
-      if (!isFiltered(p)) {
-        shitpost = false;
-        break;
-      }
-    }
-    if (shitpost) {
-      removePost(post);
-      return;
-    }
-  }
+	if (post == null || isFiltered(post))
+		return;
+	
+	let parents = getParents(post);
+	if (parents.length > 0) {
+		let shitpost = true;
+		for (let p of parents) {
+			let parent;
+			if (p[1] == post.op)
+				parent = postList.get(p[0]);
+			else
+			parent = postList.getFromAll(p[0]);
+			if (!isFiltered(parent)) {
+				shitpost = false;
+				break;
+			}
+		}
+		if (shitpost) {
+			removePost(post);
+		}
+	}
 }
 
 function removePost(post)  {
-  if (invisibleDelete) {
-    var oldAttr = post.getAttribute("style");
-    if (oldAttr == null)
-      oldAttr = "";
-    post.setAttribute("style", oldAttr + "display: none;");
-  } else {
-    if (!post.className.includes("deleted"))
-      post.className += " deleted";
-  }
-  
-  // Mark as custom filtered class for convenience
-  post.className += REMOVED;
-     
-  // Increase and update filter count
-  deletedPostCount++;
-  updateRemovedCounter();
-}
-
-function getPostName(post) {
-  // Need to search by class because children of the post seem to differ between observer and manual call
-  return post.childNodes[1].getElementsByClassName("name")[0].childNodes[0].innerHTML;
-}
-
-function getReplies(post) {
-  //var backlinks = Array.prototype.slice.call(post.childNodes[post.childNodes.length - 1].getElementsByClassName("post-link"));
-  var backLinkSpan = post.getElementsByClassName("backlinks");
-  if (backLinkSpan == null || backLinkSpan.length == 0)
-    return [];
-  var backlinks = Array.prototype.slice.call(backLinkSpan[0].getElementsByClassName("post-link"));
-  
-  // Replace backlink elements with real posts
-  for (var i = 0; i < backlinks.length; i++) {
-    backlinks[i] = getPostWithID(backlinks[i].getAttribute("data-id"))
-  }
-  
-  return backlinks;
+	if (invisibleDelete) {
+		post.remove()
+	} else {
+		post.setDeleted();
+	}
+	
+	// Increase and update filter count
+	if (!hideFilterCount) {
+		deletedPostCount++;
+		updateRemovedCounter();
+	}
 }
 
 function getParents(post) {
-  var postContainer = post.getElementsByClassName("post-container")[0];
-  var parents = Array.prototype.slice.call(postContainer.getElementsByClassName("post-link"));
-  
-  for (var i = 0; i < parents.length; i++) {
-    parents[i] = getPostWithID(parents[i].getAttribute("data-id"));
-  }
-  return parents;
+	let parents = post.links;
+	if (parents == null)
+		return []
+	return parents;
 }
 
-function getPostWithID(id) {
-  return document.getElementById("p" + id);
+function getPostFromDOMObject(p) {
+	return postList.get(p.getAttribute('id').slice(1));
 }
 
 function isFiltered(post) {
-  // Might be null if not shown (eg when only showing last 100 posts)
-  if (post == null)
-    return false;
-  return post.className.includes(REMOVED);
+	if (post == null)
+		return invisibleDelete;
+
+	if (invisibleDelete)
+		return !postList.has(post.id);
+	else
+		return post.deleted;
 }
 
 function createRemovedCounter() {
-  var removedCountContent = document.createTextNode(deletedPostCount);
-  removedCountElement = document.createElement("b");
-  removedCountElement.setAttribute("class", "act hide-empty banner-float");
-  removedCountElement.setAttribute("title", "Removed Posts");
-  removedCountElement.appendChild(removedCountContent);
-  document.getElementById("banner").insertBefore(removedCountElement, document.getElementById("thread-post-counters"));
+	var removedCountContent = document.createTextNode(deletedPostCount);
+	removedCountElement = document.createElement("b");
+	removedCountElement.setAttribute("class", "act hide-empty banner-float");
+	removedCountElement.setAttribute("title", "Removed Posts");
+	removedCountElement.appendChild(removedCountContent);
+	document.getElementById("banner").insertBefore(removedCountElement, document.getElementById("thread-post-counters"));
 }
 
 function updateRemovedCounter() {
-  removedCountElement.innerHTML = deletedPostCount;
+	removedCountElement.innerHTML = deletedPostCount;
 }
 
 // ================== SCRIPT ==================
 
-newPostObserver   = new MutationObserver(postCreationHandler);
-var obsConfig         = { childList: true };
 
 // Create Filtered Counter on top
 if (!hideFilterCount)
-    createRemovedCounter();
+	createRemovedCounter();
 
-// Start new post observer
-newPostObserver.observe(threadContainer, obsConfig);
-
-// Delete old posts
-var oldPosts = Array.prototype.slice.call(threadContainer.getElementsByTagName('article'));
-opPost = oldPosts.shift(); // Ignore OP
-for (var post of oldPosts)
-  checkForRemoval(post);
+// Wait for Meguca to finish loading
+var finLoadingHandler = function(e) { 
+	// Get all old posts and check for removal
+	postList = window.require("state").posts;
+	for (let post of postList) {
+		checkForRemovalByName(post);
+	}
+	for (let post of postList) {
+		checkForRemovalByReplies(post);
+	}
+	
+	// Using DOM to get notified on new posts; Can't find a good way to use the Meguca API
+	newPostObserver	= new MutationObserver(postCreationHandler);
+	newPostObserver.observe(threadContainer, { childList: true });
+};
+var finLoadingObserver = new MutationObserver(finLoadingHandler);
+finLoadingObserver.observe(document.getElementById('loading-image'), {attributes: true});
